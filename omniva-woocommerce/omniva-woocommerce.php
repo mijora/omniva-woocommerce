f48775c4d6db0499e47e25dd18e5556b474357d1
Rimvydas <lithstud@gmail.com>
2020-04-29 18:04:24 +0300

    version bump

--- omniva-woocommerce/omniva-woocommerce.php
+++ omniva-woocommerce/omniva-woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: Omniva shipping
  * Description: Omniva shipping plugin for WooCommerce
  * Author: Omniva
- * Version: 1.5.6
+ * Version: 1.5.7
  * Domain Path: /languages
  * Text Domain: omnivalt
  * WC requires at least: 3.0.0
