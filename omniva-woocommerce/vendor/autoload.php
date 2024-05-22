dbc36ece8f690f825bc7853e9978112750906a47
Marijus Kundelis <markakk@gmail.com>
2024-05-22 10:35:42 +0300

    Refresh vendors

--- omniva-woocommerce/vendor/autoload.php
+++ omniva-woocommerce/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInita57161de2b10c8687e3d1a12a18328aa::getLoader();
+return ComposerAutoloaderInita73aa8f43670ba7945f8b18cff7bb9d1::getLoader();
